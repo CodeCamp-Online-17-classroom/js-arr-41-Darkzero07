d5a219fa79da21223e0d8fec5c3ee279a663e417
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-21 16:09:33 +0700

    js lab 127

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+// ให้สร้าง array result ให้ได้ผลลัพธ์ตามเงื่อนไขที่กำหนด โดยใช้ arr.filter
+
+const array = [
+  { id: 1, name: 'Pepsi' },
+  { id: 2, name: 'Mirinda' },
+  { id: 3, name: 'Coke' },
+  { id: 4, name: 'Fanta' },
+  { id: 5, name: 'Sprite' }
+];
+
+const result = array.filter(arr => {
+    if (arr.id !== 4) {
+        return arr
+    }
+})
+console.log(result)
+// id ไม่เท่ากับ 4
